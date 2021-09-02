12b2b768a6ac8b88142f25fe9d0002cbeb5ee97d
rayyan <rayyanshaikh54321@gmail.com>
2021-09-02 15:59:29 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/module1.mjs
@@ -0,0 +1,3 @@
+import data ,{a,b,c}from "./module2.mjs";
+console.log(data);
+console.log(b)
---
+++ src/module2.mjs
@@ -0,0 +1,7 @@
+let a="rayyan";
+let b="faizan";
+let c="saliq";
+ export default a;
+ export {a};
+ export {b};
+ export {c};
